a139d00cda3474eda66d61a6644ea438fb31ee51
Tiago Vinícius <tiagovinicyus1996@gmail.com>
2023-01-17 16:54:33 +0000

    commit

---
+++ EXERCÍCIOS JAVASCRIPT/ex007 Condições/ex001.js
@@ -0,0 +1,6 @@
+var velo = 70
+if (velo > 60) { // Condição Simples
+    console.log(`Sua velocidade é de ${velo}km/h e a máxima permitida é de 60km/h. Foste MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
+
