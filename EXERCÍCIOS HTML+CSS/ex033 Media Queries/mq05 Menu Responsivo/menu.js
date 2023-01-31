123fe07f2c87f0c7560c9dd2f21a25c5b7c813a5
Tiago Vinícius <tiagovinicyus1996@gmail.com>
2023-01-31 16:06:02 +0000

    site media query pronto

---
+++ EXERCÍCIOS HTML+CSS/ex033 Media Queries/mq05 Menu Responsivo/menu.js
@@ -0,0 +1,18 @@
+function clickMenu() {
+    
+        if (menu.style.display == 'block'){
+            menu.style.display = 'none'
+        } else {
+            menu.style.display = 'block'
+        }
+
+}
+
+function mudouTamanho() {
+    
+    if (window.innerWidth >= 768) {
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+}
